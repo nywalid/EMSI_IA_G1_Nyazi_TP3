4bf4672fdc5cf068a584a8376882277fdf877070
walid <walidnyazi17@gmail.com>
2025-11-08 15:30:43 +0100

    Creation projet NYAZI

---
+++ src/main/java/ma/emsi/nyazi/tp3_nyazi/HelloApplication.java
@@ -0,0 +1,9 @@
+package ma.emsi.nyazi.tp3_nyazi;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/ma/emsi/nyazi/tp3_nyazi/HelloResource.java
@@ -0,0 +1,14 @@
+package ma.emsi.nyazi.tp3_nyazi;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
