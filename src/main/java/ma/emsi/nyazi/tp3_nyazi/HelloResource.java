d92c77b16fb2572b64220f4c4415083cfd95b48b
walid <walidnyazi17@gmail.com>
2025-11-08 15:54:03 +0100

    Modification fonction hello NYAZI

--- src/main/java/ma/emsi/nyazi/tp3_nyazi/HelloResource.java
+++ src/main/java/ma/emsi/nyazi/tp3_nyazi/HelloResource.java
@@ -2,13 +2,15 @@ package ma.emsi.nyazi.tp3_nyazi;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 
 @Path("/hello-world")
 public class HelloResource {
     @GET
+    @Path("personnes/{nom}")
     @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    public String hello(@PathParam("nom")String nom) {
+        return "Hello, "+nom;
     }
 }
